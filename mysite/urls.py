9cab792c5605aac04ff2412d3e8e8d873cf9c2dd
Gtihon9 <gtihon9@gmail.com>
2022-05-13 23:18:36 +0300

    Draft messages(post, edit, delete). auth

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accaounts/login/', views.LoginView.as_view(), name='login'),
     path('', include('blog.urls')),
 ]
